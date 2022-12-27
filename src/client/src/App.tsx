9f2f5ecdccf163b7f2e2ea836b03b0da75ccfb1d
Mitchell Hay <haym23@yahoo.com>
2022-12-27 18:37:29 -0500

    Javascript no errors

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import './App.css';
 import { SlotMachine } from './components';
-import icon from './images/favicon.ico';
-import cherry from './images/cherry.gif';
 
 function App() {
-  const images = [
-    icon,
-    cherry
-  ];
 
   return (
     <div className="App">
-      <SlotMachine images={images}/>
+      <SlotMachine />
     </div>
   );
 }
