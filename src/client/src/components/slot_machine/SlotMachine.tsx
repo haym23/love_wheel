5c53655aa95377233d9d406190a8f45147693186
Mitchell Hay <haym23@yahoo.com>
2023-02-16 18:41:35 -0500

    Working slot machine with JS

--- src/client/src/components/slot_machine/SlotMachine.tsx
+++ src/client/src/components/slot_machine/SlotMachine.tsx
@@ -30,8 +30,9 @@ const items = [
  *  using the above items
  */
 const SlotMachine: React.FC = () => {
-  const [duration, setDuration] = useState(5);
+  const [durations] = useState([1, 2, 3]);
   const doors = useRef<HTMLDivElement>(null);
+  const nameplate = useRef<HTMLDivElement>(null);
 
   /**
    * init Function
@@ -40,70 +41,73 @@ const SlotMachine: React.FC = () => {
    *  (❓)
    */
   function init() {
+    initNames();
+
     // Shallow copy items
     const arr = items;
 
     if (doors.current) {
       const divs = doors.current.querySelectorAll('.door') as NodeListOf<HTMLElement>;
-      divs.forEach((div) => {
+      divs.forEach((div, index) => {
         const reel = div.querySelector('.reel') as HTMLElement;
         const reelClone = reel.cloneNode(false) as HTMLElement;
         const pool = ['❓'];
 
         pool.push(...shuffle(arr));
 
-        reelClone.addEventListener(
-          'transitionstart',
-          () => {
-            const imgDivs = div.querySelectorAll('.image') as NodeListOf<HTMLElement>;
-            imgDivs.forEach((img) => {
-              img.style.filter = 'blur(3px)';
-              const blur_transition = `filter ${duration * 0.1}s ease-in`;
-              const spin_transition = `transform ${duration * 0.5}s ease-in-out`;
-              img.style.transition = `${ blur_transition }, ${ spin_transition }`;
-            });
-          },
-          { once: true },
-        );
-
-        setDuration(parseInt(reel.style.transitionDuration));
-
         for (let i = pool.length - 1; i >=0; --i) {
-          const box = document.createElement('div');
-          box.classList.add('image');
-          box.style.width = div.clientWidth + 'px';
-          box.style.height = div.clientHeight + 'px';
-          box.textContent = pool[i];
-          reelClone.appendChild(box);
+          const imgDiv = document.createElement('div');
+          imgDiv.classList.add('image');
+          imgDiv.style.width = div.clientWidth + 'px';
+          imgDiv.style.height = div.clientHeight + 'px';
+          imgDiv.textContent = pool[i];
+          reelClone.appendChild(imgDiv);
         }
-        reelClone.style.transitionDuration = `${duration > 0 ? duration : 1}s`;
+
+        reelClone.style.transitionDuration = `${durations[index] > 0 ? durations[index] : 1}s`;
         reelClone.style.transform = `translateY(-${div.clientHeight * (pool.length - 1)}px)`;
         div.replaceChild(reelClone, reel);
       });
     }
   }
 
-  function blur() {
-    console.log('Got to blur');
-    if (doors.current) {
-      const divs = doors.current.querySelectorAll('.door') as NodeListOf<HTMLElement>;
-      divs.forEach((div) => {
-        const imageDivs = div.querySelectorAll('.image') as NodeListOf<HTMLElement>;
-        imageDivs.forEach((img, index) => {
-          img.style.filter = 'blur(0)';
-          img.style.transition = `filter ${duration * 0.5}s ease-in`;
-        });
-      });
+  /**
+   * init names Function
+   * 
+   * @description Reset the nameplate for later
+   *  (❓)
+   */
+  function initNames() {
+    if (nameplate.current) {
+      console.log(nameplate.current);
+      const name = nameplate.current.querySelector('.reel') as HTMLElement;
+      const nameClone = name.cloneNode(false) as HTMLElement;
+
+      const pool = ['Name'];
+
+      pool.push(...shuffle(['Mitchell', 'Sam', 'Todd']));
+
+      for (let i = pool.length - 1; i >=0; --i) {
+        const imgDiv = document.createElement('div');
+        imgDiv.classList.add('image');
+        imgDiv.style.width = nameplate.current.clientWidth + 'px';
+        imgDiv.style.height = nameplate.current.clientHeight + 'px';
+        imgDiv.textContent = pool[i];
+        nameClone.appendChild(imgDiv);
+      }
+
+      nameClone.style.transitionDuration = `3s`;
+      nameClone.style.transform = `translateY(-${nameplate.current.clientHeight * (pool.length - 1)}px)`;
+      nameplate.current.replaceChild(nameClone, name);
     }
   }
 
-  async function spin() {
+  function spin() {
     if (doors.current) {
       const divs = doors.current.querySelectorAll('.door') as NodeListOf<HTMLElement>;
       divs.forEach((div) => {
         const reel = div.querySelector('.reel') as HTMLElement;
         reel.style.transform = 'translateY(0)'
-        return new Promise((resolve) => setTimeout(resolve, duration * 500));
       });
     }
   }
@@ -123,18 +127,24 @@ const SlotMachine: React.FC = () => {
 
   return (
     <div id="app">
+
+      <div className="nameplate" ref={nameplate}>
+        <div className="reel">
+        </div>
+      </div>
+
       <div className="doors" ref={doors}>
-        <div className="door" onTransitionEnd={blur}>
+        <div className="door">
           <div className="reel">
           </div>
         </div>
 
-        <div className="door" onTransitionEnd={blur}>
+        <div className="door">
           <div className="reel">
           </div>
         </div>
 
-        <div className="door" onTransitionEnd={blur}>
+        <div className="door">
           <div className="reel">
           </div>
         </div>
